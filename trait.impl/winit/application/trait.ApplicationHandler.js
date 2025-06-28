d393c6fa36473c74caaeb4c6b0bd16fe4e82fbcc
James <JesusButForGayPeople@proton.me>
2025-06-28 02:27:49 -0400

    Update documentation

--- trait.impl/winit/application/trait.ApplicationHandler.js
+++ trait.impl/winit/application/trait.ApplicationHandler.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mirador",[["impl <a class=\"trait\" href=\"winit/application/trait.ApplicationHandler.html\" title=\"trait winit::application::ApplicationHandler\">ApplicationHandler</a> for <a class=\"struct\" href=\"mirador/app/struct.App.html\" title=\"struct mirador::app::App\">App</a>"]]],["winit",[]]]);
+    var implementors = Object.fromEntries([["mirador",[["impl ApplicationHandler for <a class=\"struct\" href=\"mirador/app/struct.App.html\" title=\"struct mirador::app::App\">App</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[280,13]}
+//{"start":57,"fragment_lengths":[145]}
