ba89615c842ef2bf9dad3e4061293491da657455
James <JesusButForGayPeople@proton.me>
2025-07-05 18:00:43 -0400

    Update documentation

--- mirador/math/vec/sidebar-items.js
+++ mirador/math/vec/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Vec3"]};
+window.SIDEBAR_ITEMS = {"struct":["Vec2","Vec3"]};
--- trait.impl/core/ops/arith/trait.Mul.js
+++ trait.impl/core/ops/arith/trait.Mul.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mirador",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"mirador/math/vec/struct.Vec3.html\" title=\"struct mirador::math::vec::Vec3\">Vec3</a>"]]]]);
+    var implementors = Object.fromEntries([["mirador",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"mirador/math/vec/struct.Vec2.html\" title=\"struct mirador::math::vec::Vec2\">Vec2</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"mirador/math/vec/struct.Vec3.html\" title=\"struct mirador::math::vec::Vec3\">Vec3</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[380]}
+//{"start":57,"fragment_lengths":[747]}
